fb9ce83c31a5e32b0a305ae13d8bdc4f18f8d7d7
soP0ggers <carlo.batac.0803@gmail.com>
2022-11-12 13:25:57 +0800

    Added comments to votes.js

--- models/votes.js
+++ models/votes.js
@@ -4,6 +4,8 @@ const votesSchema = new mongoose.Schema({
     postID:{type: String, required: true},
     username: {type: String, required: true},
     vote: {type: Boolean, required: true},
+        // If vote === 0 -> Downvote
+        // If vote === 1 -> Upvote 
 });
 
 module.exports = mongoose.model('Votes', votesSchema);
