80f2039548ab0d300a7c19586b0962be271496c0
soP0ggers <carlo.batac.0803@gmail.com>
2022-11-15 18:09:43 +0800

    Updated voteBtn.js

--- public/js/createView.js
+++ public/js/createView.js
@@ -1,11 +1,13 @@
 const srcInput = document.querySelector("#create-post-input-content");
 var srcUploaded = "";
 
-srcInput.addEventListener("change", function(){
-    const reader = new FileReader();
-    reader.addEventListener("load", () => {
-        srcUploaded = reader.result;
-        document.querySelector("#create-post-content").style.backgroundImage=`url(${srcUploaded})`;
-    });
-    reader.readAsDataURL(this.files[0]);
-})
+if (srcInput){
+    srcInput.addEventListener("change", function(){
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+            srcUploaded = reader.result;
+            document.querySelector("#create-post-content").style.backgroundImage=`url(${srcUploaded})`;
+        });
+        reader.readAsDataURL(this.files[0]);
+    })
+}
--- public/js/voteBtn.js
+++ public/js/voteBtn.js
@@ -2,10 +2,30 @@
 
 $("#upVote").click(function(){
     var temp = $("#post-upv-count").text();
-    $("#post-upv-count").text(parseInt(temp) + 1);
-    $("#post-upv").addClass("bi bi-caret-up-fill").removeClass("bi bi-caret-up");
+    // if class is up-fill, then remove vote
+    if($("#post-upv").hasClass("bi-caret-up-fill")){
+        $("#post-upv-count").text(parseInt(temp) - 1);
+        $("#post-upv").addClass("bi-caret-up").removeClass("bi-caret-up-fill");
+    }else if($("#post-upv").hasClass("bi-caret-up")){
+        $("#post-upv-count").text(parseInt(temp) + 1);
+        $("#post-upv").addClass("bi-caret-up-fill").removeClass("bi-caret-up");
+    }
+    
 });
+
 $("#downVote").click(function(){
     var temp = $("#post-dv-count").text();
-    $("#post-dv-count").text(parseInt(temp) - 1);
+    // if class is down-fill, then remove vote
+    if($("#post-dv").hasClass("bi-caret-down-fill")){
+        $("#post-dv-count").text(parseInt(temp) - 1);
+        $("#post-dv").addClass("bi-caret-down").removeClass("bi-caret-down-fill");
+    }else if($("#post-dv").hasClass("bi-caret-down")){
+        $("#post-dv-count").text(parseInt(temp) + 1);
+        $("#post-dv").addClass("bi-caret-down-fill").removeClass("bi-caret-down");
+    }
+    
 });
+
+function noReload(){
+    event.preventDefault();
+}
