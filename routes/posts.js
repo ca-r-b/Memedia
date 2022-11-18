814cb9c6b0cdc90e7c8b3bf2d35edd963ec4ba25
soP0ggers <carlo.batac.0803@gmail.com>
2022-11-18 23:25:40 +0800

    Report Post - Working
    Only "username" of reporter being passed is missing

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const path = require('path');
+const RepComment = require("../models/reportComments");
+const RepPost = require("../models/reportPosts");
 
 // TO-DO - DATABASE NEEDED: Add .post of routes
+
+
+// ============== Post Uploading ==============
 
 router.get("/postCreate", function(req, res){
     res.render("postCreate", {title: "Post Your Meme"});
@@ -14,7 +19,33 @@ router.post("/postUpload", function(req, res){
 
     // Redirect to upload folder
     img.mv(path.resolve(__dirname + '/..', 'public/images/posts', imgName));
+
+    // Adding to Database here***
 });
+
+// ============== Post Reporting ==============
+
+router.post("/postReport/:id", function(req, res){
+    var repType = req.body.reportType;
+
+    const report = new RepPost({
+        postID: req.params.id,
+        // Replace "reporterUser with Logged-In User"
+        reporterUser: "pop", 
+        remarks: repType
+    });
+
+    report.save()
+        .then((result) => {
+            res.redirect(req.get("referer"));
+        })
+        .catch((err) =>{
+            res.send(err);
+        });
+
+});
+
+// ============== Post Viewing ==============
 
 router.get("/postView", function(req, res){
     res.render("postView", {title: "Your Main Source of Fun"});
